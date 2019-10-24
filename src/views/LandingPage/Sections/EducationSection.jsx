8e5b60e469b6d6f73caa2585730cd409ffa3feff
Nick Paridon <paridonn1@gmail.com>
2019-10-24 12:16:10 -0400

    Formatting fix

--- src/views/LandingPage/Sections/EducationSection.jsx
+++ src/views/LandingPage/Sections/EducationSection.jsx
@@ -19,14 +19,14 @@ class EducationSection extends React.Component {
 				</GridContainer>
 				<div>
 					<GridContainer>
-						{/* <GridItem xs={12} sm={12} md={4}>
-							<InfoArea
+						<GridItem xs={12} sm={12} md={4}>
+							{/* <InfoArea
 								title="University of Pennsylvania"
 								description="2018 - Six month course focused around  fullstack JavaScript. Learned Node.JS on the backend and continued working with React.Js on the frontend"
 								icon={upenn}
 								iconColor="danger"
 								vertical
-							/>
+							/> */}
 						</GridItem> */}
 						<GridItem xs={12} sm={12} md={4}>
 							<InfoArea
@@ -37,15 +37,15 @@ class EducationSection extends React.Component {
 								vertical
 							/>
 						</GridItem>
-						{/* <GridItem xs={12} sm={12} md={4}>
-							<InfoArea
+						<GridItem xs={12} sm={12} md={4}>
+							{/* <InfoArea
 								title="Launch Academy"
 								description="Fullstack Ruby on Rails program. Went through the fundamentals of things like test driven development and object oriented programming."
 								icon={launch}
 								iconColor="success"
 								vertical
-							/>
-						</GridItem> */}
+							/> */}
+						</GridItem>
 					</GridContainer>
 				</div>
 			</div>
