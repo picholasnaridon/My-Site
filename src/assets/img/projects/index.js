dce66c277ef4b0618e956dd974bdb934a72ea7c3
Nick Paridon <nparidon@frontlineed.com>
2021-06-28 16:01:46 -0400

    Promotion 6/28/21
    Also deletes a bunch of stuff that was old

--- src/assets/img/projects/index.js
+++ src/assets/img/projects/index.js
@@ -1,25 +1,5 @@
-import starwarspic from './starwars.png';
-import hearthstonepic from './hearthstone.png';
-import giphypic from './giphy.png';
-import hangmanpic from './hangman.png';
-import rpspic from './rps.png';
-import liripic from './liri.png';
-import friendfinderpic from './friendfinder.png';
-import burgerSQL from './burgerSQL.png';
-import savethedate from './savethedate.png';
-import nytreact from './NYTBlack.png';
 import followup from './followUpBlack.png';
 
 export {
-	starwarspic,
-	hearthstonepic,
-	giphypic,
-	hangmanpic,
-	rpspic,
-	liripic,
-	friendfinderpic,
-	burgerSQL,
-	savethedate,
-	nytreact,
 	followup
 };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,7 @@
 import LandingPage from "views/LandingPage/LandingPage.jsx";
-import StarWars from "views/StarWars/StarWars";
-import Giphy from "views/Giphy/Giphy"
-import Board from "views/Hangman/Board"
 
 var indexRoutes = [
-  { path: "/", name: "LandingPage", component: LandingPage },
-  { path: "/starwars", name: "StarWars", component: StarWars },
-  { path: "/giphy", name: "Giphy", component: Giphy },
-  { path: "/rps", name: "RockPaperScissors", component: StarWars },
-  { path: "/hangman", name: "Hangman", component: Board }
+  { path: "/", name: "LandingPage", component: LandingPage }
 ];
 
 export default indexRoutes;
